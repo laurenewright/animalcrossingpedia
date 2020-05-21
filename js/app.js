06c62db578f01ff5d6c9af355d798871467b0d58
Lauren Wright <wright.lauren20@gmail.com>
2020-05-21 11:17:33 -0400

    planned out what to do and set up server file

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//document.querySelector(".example").style.backgroundColor = "red";
+const myFunction = function myFunction() {
+    document.querySelector(".example").style.backgroundColor = "red";
+  }
+
+  exports myFunction = myFunction;
