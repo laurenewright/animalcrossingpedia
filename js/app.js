588a54d3d369179165493cc0564e692f8566e6ba
Lauren Wright <wright.lauren20@gmail.com>
2020-05-23 22:12:50 -0400

    fetched bugs and fish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-//document.querySelector(".example").style.backgroundColor = "red";
-const myFunction = function myFunction() {
-    document.querySelector(".example").style.backgroundColor = "red";
-  }
+document.querySelector(".example").style.backgroundColor = "red";
+// const myFunction = function myFunction() {
+//     document.querySelector(".example").style.backgroundColor = "red";
+//   }
 
-  exports myFunction = myFunction;
+  //exports myFunction = myFunction;
+
+
+const fishEndpoint = 'http://acnhapi.com:1304/v1/fish';
+const fishData = fetch(fishEndpoint)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+const bugsEndpoint = 'http://acnhapi.com:1304/v1/bugs/';
+const bugData = fetch(bugsEndpoint)
+        .then(response => response.json())
+        .then(data => console.log(data));
+// const fishEndpoint = 'http://acnhapi.com:1304/v1/fish';
+// const fishData = fetch(fishEndpoint)
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+
+
+var fishDiv = document.getElementById("fishDiv"); 
+document.body.insertBefore(newDiv, currentDiv); 
+}
