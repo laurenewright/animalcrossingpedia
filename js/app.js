a173db58e9ca4ece161ac35a7ae33f06bac2aec2
Lauren Wright <wright.lauren20@gmail.com>
2020-06-07 17:02:52 -0400

    fetch and trying to append

--- js/app.js
+++ js/app.js
@@ -1,26 +1,38 @@
-document.querySelector(".example").style.backgroundColor = "red";
-// const myFunction = function myFunction() {
-//     document.querySelector(".example").style.backgroundColor = "red";
-//   }
-
-  //exports myFunction = myFunction;
+// fetch fish data
 
 
-const fishEndpoint = 'http://acnhapi.com:1304/v1/fish';
+let fishEndpoint = 'http://acnhapi.com/v1a/fish';
 const fishData = fetch(fishEndpoint)
         .then(response => response.json())
         .then(data => console.log(data));
 
-const bugsEndpoint = 'http://acnhapi.com:1304/v1/bugs/';
-const bugData = fetch(bugsEndpoint)
-        .then(response => response.json())
-        .then(data => console.log(data));
-// const fishEndpoint = 'http://acnhapi.com:1304/v1/fish';
-// const fishData = fetch(fishEndpoint)
+// fish.forEach(fish => {
+//         console.log(fishData)
+// });
+
+// Object.keys(fishData).forEach(function (key){
+//         console.log(fishData[key]);
+//     });
+
+// const fish = document.getElementById("fishDiv");
+// fish.append(fishData);      
+
+// document.getElementById("fishDiv".innerHTML = 'Name: ' + fishData);
+function appendFish(data){
+        for (var i = 0; i < data.length; i++) {
+                // append each person to our page
+                var div = document.createElement();
+                div.innerHTML = data[i];
+                document.getElementById("#fishDiv").appendChild(data);
+                }
+        }
+
+// for (var i=0; i<fishData.length; i++) {
+//         document.querySelectorAll('#fishDiv')[i].innerHTML = fishData[i];
+//       }
+
+//fetch bug data
+// const bugsEndpoint = 'http://acnhapi.com/v1a/bugs';
+// const bugData = fetch(bugsEndpoint)
 //         .then(response => response.json())
 //         .then(data => console.log(data));
-
-
-var fishDiv = document.getElementById("fishDiv"); 
-document.body.insertBefore(newDiv, currentDiv); 
-}
