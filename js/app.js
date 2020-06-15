0a8d4aa3f7372066f6d3a4a697f2e750623b4a7d
Lauren Wright <wright.lauren20@gmail.com>
2020-06-15 18:29:39 -0400

    fetched both bugs and fish

--- js/app.js
+++ js/app.js
@@ -4,35 +4,33 @@
 let fishEndpoint = 'http://acnhapi.com/v1a/fish';
 const fishData = fetch(fishEndpoint)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then((data) => {
+                console.log(data);
+                appendFish(data);
+        })
 
-// fish.forEach(fish => {
-//         console.log(fishData)
-// });
-
-// Object.keys(fishData).forEach(function (key){
-//         console.log(fishData[key]);
-//     });
-
-// const fish = document.getElementById("fishDiv");
-// fish.append(fishData);      
-
-// document.getElementById("fishDiv".innerHTML = 'Name: ' + fishData);
 function appendFish(data){
         for (var i = 0; i < data.length; i++) {
                 // append each person to our page
-                var div = document.createElement();
-                div.innerHTML = data[i];
-                document.getElementById("#fishDiv").appendChild(data);
+                var div = document.createElement("div");
+                div.innerHTML = data[i].name["name-USen"];
+                document.getElementById("fishDiv").appendChild(div);
                 }
         }
 
-// for (var i=0; i<fishData.length; i++) {
-//         document.querySelectorAll('#fishDiv')[i].innerHTML = fishData[i];
-//       }
+let bugEndpoint = 'http://acnhapi.com/v1a/bugs';
+const bugData = fetch(bugEndpoint)
+        .then(response => response.json())
+        .then((data) => {
+                console.log(data);
+                appendBug(data);
+        })
 
-//fetch bug data
-// const bugsEndpoint = 'http://acnhapi.com/v1a/bugs';
-// const bugData = fetch(bugsEndpoint)
-//         .then(response => response.json())
-//         .then(data => console.log(data));
+function appendBug(data){
+        for (var i = 0; i < data.length; i++) {
+                // append each person to our page
+                var div = document.createElement("div");
+                div.innerHTML = data[i].name["name-USen"];
+                document.getElementById("bugDiv").appendChild(div);
+                }
+        }
