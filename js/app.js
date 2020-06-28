c57655e2af1257ea6090033bca4626d0cf078976
Lauren Wright <wright.lauren20@gmail.com>
2020-06-27 22:11:15 -0400

    bug buttons = bug info fish buttons = fish info

--- js/app.js
+++ js/app.js
@@ -5,19 +5,34 @@ const fishNames = fetch(fishEndpoint)
         .then((data) => {
                 console.log(data);
                 appendFishName(data);
-                //appendFishInfo(data);
+                appendFishInfo(data);
         })
 // append names
 function appendFishName(data){
         for (var i = 0; i < data.length; i++) {
                 var div = document.createElement("button");
                 div.innerHTML = data[i].name["name-USen"];
-                div.setAttribute("class", "critterButton");
+                div.setAttribute("class", "critterButtonFish");
                 //set alt text for reader as the name of critter
                 document.getElementById("fishDiv").appendChild(div);
                 }
         }
 
+function appendFishInfo(data){
+        for (var i = 0; i < data.length; i++) {
+                var div1 = document.createElement("p");
+                var div2 = document.createElement("p");
+                var div3 = document.createElement("p");
+                div1.innerHTML = data[i].name["name-USen"];
+                div2.innerHTML = data[i].price;
+                div3.innerHTML = data[i]["museum-phrase"];
+                div1.setAttribute("class", "critterInfo fish");
+                //set alt text for reader as the name of critter
+                document.getElementById("fishInfo").appendChild(div1);
+                document.getElementById("fishInfo").appendChild(div2);
+                document.getElementById("fishInfo").appendChild(div3);
+        }
+}
 
 //fetch bug data
 let bugEndpoint = 'http://acnhapi.com/v1a/bugs';
@@ -33,7 +48,7 @@ function appendBugName(data){
         for (var i = 0; i < data.length; i++) {
                 var div = document.createElement("button");
                 div.innerHTML = data[i].name["name-USen"];
-                div.setAttribute("class", "critterButton");
+                div.setAttribute("class", "critterButtonBug");
                 //set alt text for reader as the name of critter
                 document.getElementById("bugDiv").appendChild(div);
                 }
@@ -44,65 +59,73 @@ function appendBugName(data){
 //fetch and display critter info when critterButton is clicked
 function appendBugInfo(data){
         for (var i = 0; i < data.length; i++) {
-                var div = document.createElement("p");
-                document.getElementById("bugInfo").innerHTML = data[i].name["name-USen"];
-                div.setAttribute("class", "critterInfo");
+                var div1 = document.createElement("p");
+                var div2 = document.createElement("p");
+                var div3 = document.createElement("p");
+                div1.innerHTML = data[i].name["name-USen"];
+                div2.innerHTML = data[i].price;
+                div3.innerHTML = data[i]["museum-phrase"];
+                div1.setAttribute("class", "critterInfo bug");
                 //set alt text for reader as the name of critter
-                document.getElementById("bugInfo").appendChild(div);
+                document.getElementById("bugInfo").appendChild(div1);
+                document.getElementById("bugInfo").appendChild(div2);
+                document.getElementById("bugInfo").appendChild(div3);
                 }
         }
 
 
-// function appendFishInfo(data){
-//         for (var i = 0; i < data.length; i++) {
-//                 var div = document.createElement("div");
-//                 div.innerHTML = data[i]["museum-name"];
-//                 div.setAttribute("class", "critterInfo");
-//                 //set alt text for reader as the name of critter
-//                 document.getElementById("fishInfo").appendChild(div);
-//                 }
-//         }
-
-        // function myFunction(data) {
-        //         document.getElementById("bugInfo").innerHTML = data[i]["name-USen";
-        //     }
 
 
-//display model when critterButton is clicked
-var critterButton = document.getElementsByClassName("critterButton");
-function onClickAlert(){
-        for (var i=0; i < critterButton.length; i++) {
-                critterButton[i].onclick = function(){
-                        modal.style.display = "block";
+//display modal when critterButton is clicked
+var critterButtonFish = document.getElementsByClassName("critterButtonFish");
+function onClickAlertFish(){
+        for (var i=0; i < critterButtonFish.length; i++) {
+                critterButtonFish[i].onclick = function(){
+                        modalFish.style.display = "block";
+                }
+        }
+}
+
+var critterButtonBug = document.getElementsByClassName("critterButtonBug");
+function onClickAlertBug(){
+        for (var i=0; i < critterButtonBug.length; i++) {
+                critterButtonBug[i].onclick = function(){
+                        modalBug.style.display = "block";
                 }
         }
 }
 
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
- var btn = document.getElementsByClassName("critterButton");
+var modalFish = document.getElementById("myModalFish");
+var modalBug = document.getElementById("myModalBug");
 
 // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
+ var spanBug = document.getElementsByClassName("closeBug")[0];
+ var spanFish = document.getElementsByClassName("closeFish")[0];
 
 // When the user clicks on the button, open the modal
- btn.onclick = function() {
-   modal.style.display = "block";
+ critterButtonBug.onclick = function() {
+   modalBug.style.display = "block";
  }
 
+ critterButtonFish.onclick = function() {
+        modalFish.style.display = "block";
+      }
+
 // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
-   modal.style.display = "none";
+ spanBug.onclick = function() {
+   modalBug.style.display = "none";
  }
+ spanFish.onclick = function() {
+        modalFish.style.display = "none";
+      }
+     
 
 // When the user clicks anywhere outside of the modal, close it
  window.onclick = function(event) {
-   if (event.target == modal) {
-     modal.style.display = "none";
-   }
+   if (event.target == modalBug) {
+     modalBug.style.display = "none";
+   } else if (event.target == modalFish) {
+        modalFish.style.display = "none";
+      }
  }
-
-
-
