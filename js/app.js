a5e814c08a93edd7c61a388f39abe4524b1c87a8
Lauren Wright <wright.lauren20@gmail.com>
2020-07-22 13:54:25 -0400

    added caught and donated buttons to main page

--- js/app.js
+++ js/app.js
@@ -5,14 +5,20 @@ const fishNames = fetch(fishEndpoint)
         .then((data) => {
                 console.log(data);
                 fishName(data);
-                fishInfo(data);
+                // fishInfo(data);
         })
 // append fish names
 const fishName = function appendFishName(data){
         for (var i = 0; i < data.length; i++) {
-                var fishId = data[i].name["name-USen"];
+                // created button for caught or donate
+                var caughtButton = document.createElement("button");
+                var donateButton = document.createElement("button");
+                // label caught or donate button
+                caughtButton.innerHTML = "Caught";
+                donateButton.innerHTML = "Donated";
                 // creating and setting the attributes of the wrapperDiv
                 var wrapperDiv = document.createElement("div");
+                // give wrapperDiv attributes
                 wrapperDiv.setAttribute("class", "fish");
                 wrapperDiv.setAttribute("id", data[i].name["name-USen"]);
                 // creating the nameDiv and iconDiv
@@ -29,22 +35,39 @@ const fishName = function appendFishName(data){
                 // appending name and icon to wrapperDiv
                 wrapperDiv.appendChild(nameDiv);
                 wrapperDiv.appendChild(iconDiv);
+                wrapperDiv.appendChild(caughtButton);
+                wrapperDiv.appendChild(donateButton);
                 // appending wrapperDiv to fishDiv
                 document.getElementById("fishDiv").appendChild(wrapperDiv);
-                var critterFish = document.getElementsByClassName("critterFish");
-                document.getElementById(data[i].name["name-USen"]).onclick = function() {
-                        //makes all buttons clickable
-                        for (var i=0; i < critterFish.length; i++) {
-                                // show modal on click
-                                critterFish[i].onclick = function(){
-                                        modalFish.fishId.style.display = "block";
-                                        // ^ adding fishId doesn't currently work
-                                }
+                // wrapperDiv.onclick = function(event) {
+                        // let desiredFish = event.target.parentElement;
+                        // modalFish.style.display = "hide";
+                        // const fishChildren = document.getElementById("fishInfo").children;
+                        //console.log(document.getElementById("fishInfo").children);
+                        //console.log(desiredFish);
+                        //console.log(this);
+                        // if(desiredFish){
+
+                        // }
+                        donateButton.onclick = function(){
+                                var desiredFish = event.target.parentElement;
+                                var donateDiv = document.getElementById("donatedDiv");  
+                                var caughtDiv = document.getElementById("caughtDiv");  
+                                var clone1 = desiredFish.cloneNode(true);
+                                var clone2 = desiredFish.cloneNode(true);
+                                donateDiv.appendChild(clone1);
+                                caughtDiv.appendChild(clone2);
                         }
-                        // when fishName[id] is clicked show fishInfo[id] in modal
-                        // each fishName needs to be clickable
-                        // maybe use find()?
-                }
+                        caughtButton.onclick = function(){
+                                var desiredFish = event.target.parentElement;
+                                var caughtDiv = document.getElementById("caughtDiv");  
+                                var clone1 = desiredFish.cloneNode(true);
+                                caughtDiv.appendChild(clone1);
+
+                        }
+                        
+                // }
+                // var critterFish = document.getElementsByClassName("critterFish");
                 }
         }
 
@@ -54,40 +77,35 @@ const fishName = function appendFishName(data){
 // each fishName needs to be clickable
 // maybe use find()?
 
-const fishInfo = function appendFishInfo(data){
-        for (var i = 0; i < data.length; i++) {
-                var wrapperDiv = document.createElement("div");
-                wrapperDiv.setAttribute("class", "fish");
-                wrapperDiv.setAttribute("id", data[i].name["name-USen"], "hide");
-                var imgDiv = document.createElement("img");
-                var nameDiv = document.createElement("p");
-                var priceDiv = document.createElement("p");
-                var phraseDiv = document.createElement("p");
-                imgDiv.src = data[i]["image_uri"];
-                nameDiv.innerHTML = data[i].name["name-USen"];
-                priceDiv.innerHTML = data[i].price;
-                phraseDiv.innerHTML = data[i]["museum-phrase"];
-                nameDiv.setAttribute("class", "critterInfo fish");
-                //set alt text for reader as the name of critter
-                wrapperDiv.appendChild(imgDiv);
-                wrapperDiv.appendChild(nameDiv);
-                wrapperDiv.appendChild(priceDiv);
-                wrapperDiv.appendChild(phraseDiv);
-                document.getElementById("fishInfo").appendChild(wrapperDiv);
-                // document.getElementById(data[i].name["name-USen"]).onclick = function(){
-                        //console.log(data[i].name["name-USen"]);
-                        // wrapperDiv.setAttribute("class", "fish show");
-                        // console.log(this);
-                // }
-        }
-}
-
-// function revealOnClick(data){
-//         if('click',  document.getElementById(data[i].name["name-USen"])){
-//                 wrapperDiv.setAttribute("class", "fish show");
+// const fishInfo = function appendFishInfo(data){
+//         for (var i = 0; i < data.length; i++) {
+//                 var wrapperDiv = document.createElement("div");
+//                 wrapperDiv.setAttribute("class", "fish");
+//                 wrapperDiv.setAttribute("id", data[i].name["name-USen"], "hide");
+//                 var imgDiv = document.createElement("img");
+//                 var nameDiv = document.createElement("p");
+//                 var priceDiv = document.createElement("p");
+//                 var phraseDiv = document.createElement("p");
+//                 var caughtButton = document.createElement("button");
+//                 var donateButton = document.createElement("button");
+//                 imgDiv.src = data[i]["image_uri"];
+//                 nameDiv.innerHTML = data[i].name["name-USen"];
+//                 priceDiv.innerHTML = data[i].price;
+//                 phraseDiv.innerHTML = data[i]["museum-phrase"];
+//                 nameDiv.setAttribute("class", "critterInfo fish");
+//                 //var caughtButton = document.createElemment("button");
+//                 caughtButton.innerHTML = "Caught";
+//                 donateButton.innerHTML = "Donated";
+//                 //set alt text for reader as the name of critter
+//                 wrapperDiv.appendChild(imgDiv);
+//                 wrapperDiv.appendChild(nameDiv);
+//                 wrapperDiv.appendChild(priceDiv);
+//                 wrapperDiv.appendChild(phraseDiv);
+//                 wrapperDiv.appendChild(caughtButton);
+//                 wrapperDiv.appendChild(donateButton);
+//                 document.getElementById("fishInfo").appendChild(wrapperDiv);
 //         }
 // }
-
 
 
 // fetch bug data
@@ -96,100 +114,158 @@ const bugNames = fetch(bugEndpoint)
         .then(response => response.json())
         .then((data) => {
                 console.log(data);
-                appendBugName(data);
-                appendBugInfo(data);
+                bugName(data);
+                // appendBugInfo(data);
         })
 // append bug names
-function appendBugName(data){
+const bugName = function appendBugName(data){
         for (var i = 0; i < data.length; i++) {
-                var iconDiv = document.createElement("img");
-                iconDiv.src = data[i]["icon_uri"];
-                var div = document.createElement("p");
-                div.innerHTML = data[i].name["name-USen"];
-                div.setAttribute("class", "critterBug");
-                iconDiv.setAttribute("class", "critterBug");
-                //set alt text for reader as the name of critter
-                document.getElementById("bugDiv").appendChild(div);
-                document.getElementById("bugDiv").appendChild(iconDiv);
-                }
-        }
-
-// fetch and display critter info when critter is clicked
-function appendBugInfo(data){
-        for (var i = 0; i < data.length; i++) {
+                // created button for caught or donate
+                var caughtButton = document.createElement("button");
+                caughtButton.setAttribute("id", "caughtButton");
+                var donateButton = document.createElement("button");
+                donateButton.setAttribute("id", "donateButton");
+                // label caught or donate button
+                caughtButton.innerHTML = "Caught";
+                donateButton.innerHTML = "Donated";
+                // creating and setting the attributes of the wrapperDiv
                 var wrapperDiv = document.createElement("div");
+                // give wrapperDiv attributes
                 wrapperDiv.setAttribute("class", "bug");
                 wrapperDiv.setAttribute("id", data[i].name["name-USen"]);
-                var imgDiv = document.createElement("img");
+                // creating the nameDiv and iconDiv
                 var nameDiv = document.createElement("p");
-                var priceDiv = document.createElement("p");
-                var phraseDiv = document.createElement("p");
-                imgDiv.src = data[i]["image_uri"];
+                var iconDiv = document.createElement("img");
+                // giving iconDiv it's image source from api
+                iconDiv.src = data[i]["icon_uri"];
+                // giving nameDiv its crittername from api
                 nameDiv.innerHTML = data[i].name["name-USen"];
-                priceDiv.innerHTML = data[i].price;
-                phraseDiv.innerHTML = data[i]["museum-phrase"];
-                nameDiv.setAttribute("class", "critterInfo bug");
-                //set alt text for reader as the name of critter
-                document.getElementById("bugInfo").appendChild(imgDiv);
-                document.getElementById("bugInfo").appendChild(nameDiv);
-                document.getElementById("bugInfo").appendChild(priceDiv);
-                document.getElementById("bugInfo").appendChild(phraseDiv);
+                // setting class for nameDiv and iconDiv
+                nameDiv.setAttribute("class", "critterBug");
+                iconDiv.setAttribute("class", "critterBug");
+                // set alt text for reader as the name of critter
+                // appending name and icon to wrapperDiv
+                wrapperDiv.appendChild(nameDiv);
+                wrapperDiv.appendChild(iconDiv);
+                wrapperDiv.appendChild(caughtButton);
+                wrapperDiv.appendChild(donateButton);
+                // appending wrapperDiv to fishDiv
+                document.getElementById("bugDiv").appendChild(wrapperDiv);
+                // wrapperDiv.onclick = function(event) {
+                        // let desiredFish = event.target.parentElement;
+                        // modalFish.style.display = "hide";
+                        // const fishChildren = document.getElementById("fishInfo").children;
+                        //console.log(document.getElementById("fishInfo").children);
+                        //console.log(desiredFish);
+                        //console.log(this);
+                        // if(desiredFish){
+
+                        // }
+
+                        
+                // }
+                        donateButton.onclick = function(){
+                                var desiredBug = event.target.parentElement;
+                                var donateDiv = document.getElementById("donatedDiv");  
+                                var caughtDiv = document.getElementById("caughtDiv");  
+                                var clone1 = desiredBug.cloneNode(true);
+                                var clone2 = desiredBug.cloneNode(true);
+                                donateDiv.appendChild(clone1);
+                                caughtDiv.appendChild(clone2);
+                                desiredBug.outerHTML = "";
+                                document.getElementById("caughtButton").outerHTML = "";
+                        }
+                        caughtButton.onclick = function(){
+                                var desiredBug = event.target.parentElement;
+                                var caughtDiv = document.getElementById("caughtDiv");  
+                                var clone1 = desiredBug.cloneNode(true);
+                                caughtDiv.appendChild(clone1);
+                                document.getElementById("caughtButton").outerHTML = "";
+                        }
+
+
+
+
+                        // var element = document.getElementById("element-id");
+                        // element.parentNode.removeChild(element);
+
+
+
                 }
         }
 
+        // const caught = function addCaught (data){
+        //         var caughtDiv = document.getElementById("caughtDiv");
+        //         var div = document.createElement("div");
+
+
+        // }
+// fetch and display critter info when critter is clicked
+// function appendBugInfo(data){
+//         for (var i = 0; i < data.length; i++) {
+//                 var wrapperDiv = document.createElement("div");
+//                 wrapperDiv.setAttribute("class", "bug");
+//                 wrapperDiv.setAttribute("id", data[i].name["name-USen"]);
+//                 var imgDiv = document.createElement("img");
+//                 var nameDiv = document.createElement("p");
+//                 var priceDiv = document.createElement("p");
+//                 var phraseDiv = document.createElement("p");
+//                 imgDiv.src = data[i]["image_uri"];
+//                 nameDiv.innerHTML = data[i].name["name-USen"];
+//                 priceDiv.innerHTML = data[i].price;
+//                 phraseDiv.innerHTML = data[i]["museum-phrase"];
+//                 nameDiv.setAttribute("class", "critterInfo bug");
+//                 //set alt text for reader as the name of critter
+//                 document.getElementById("bugInfo").appendChild(imgDiv);
+//                 document.getElementById("bugInfo").appendChild(nameDiv);
+//                 document.getElementById("bugInfo").appendChild(priceDiv);
+//                 document.getElementById("bugInfo").appendChild(phraseDiv);
+//                 }
+//         }
 
 
 
-// display modal when critter is clicked
-// var critterFish = document.getElementsByClassName("critterFish");
-// function onClickAlertFish(){
-//         for (var i=0; i < critterFish.length; i++) {
-//                 critterFish[i].onclick = function(){
-//                         modalFish.style.display = "block";
+
+// var critterBug = document.getElementsByClassName("critterBug");
+// function onClickAlertBug(){
+//         for (var i=0; i < critterBug.length; i++) {
+//                 critterBug[i].onclick = function(){
+//                         modalBug.style.display = "block";
+//                         console.log(this);
 //                 }
 //         }
 // }
 
-var critterBug = document.getElementsByClassName("critterBug");
-function onClickAlertBug(){
-        for (var i=0; i < critterBug.length; i++) {
-                critterBug[i].onclick = function(){
-                        modalBug.style.display = "block";
-                        console.log(this);
-                }
-        }
-}
-
 // Get the modal
-var modalFish = document.getElementById("myModalFish");
-var modalBug = document.getElementById("myModalBug");
+// var modalFish = document.getElementById("myModalFish");
+// var modalBug = document.getElementById("myModalBug");
 
 // Get the <span> element that closes the modal
- var spanBug = document.getElementsByClassName("closeBug")[0];
- var spanFish = document.getElementsByClassName("closeFish")[0];
+//  var spanBug = document.getElementsByClassName("closeBug")[0];
+//  var spanFish = document.getElementsByClassName("closeFish")[0];
 
 // When the user clicks on the , open the modal
- critterBug.onclick = function() {
-   modalBug.style.display = "block";
- }
+//  critterBug.onclick = function() {
+//    modalBug.style.display = "block";
+//  }
 
 // When the user clicks on <span> (x), close the modal
- spanBug.onclick = function() {
-   modalBug.style.display = "none";
- }
- spanFish.onclick = function() {
-        modalFish.style.display = "none";
-      }
+//  spanBug.onclick = function() {
+//    modalBug.style.display = "none";
+//  }
+//  spanFish.onclick = function() {
+//         modalFish.style.display = "none";
+//       }
      
 
 // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-   if (event.target == modalBug) {
-     modalBug.style.display = "none";
-   } else if (event.target == modalFish) {
-        modalFish.style.display = "none";
-      }
- }
+//  window.onclick = function(event) {
+//    if (event.target == modalBug) {
+//      modalBug.style.display = "none";
+//    } else if (event.target == modalFish) {
+//         modalFish.style.display = "none";
+//       }
+//  }
 
 
 
